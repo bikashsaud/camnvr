35b1379f8b349e75546330ca1cf5280810c585c3
Rabin Bishwokarma <r.alpha919@gmail.com>
2021-04-21 16:56:08 +0545

    first commit

---
+++ camnvr/home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from home.views import HomeIndexView
+from django.conf import settings
+
+urlpatterns = [
+    path('', HomeIndexView.as_view()),
+    path('stream/<str:id>', views.stream, name='stream'),
+]
---
+++ camnvr/home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
+from django.views import View
+from service.camnvr_service import CamNVR
+ 
+service = CamNVR()
+service.start()
+
+class HomeIndexView(View):
+    template_name = "../templates/home.html"
+
+    def __init__(self):
+        super().__init__()
+
+    def get(self, request, *args, **kwargs):        
+        context = {"camera_details": service.get_cameras()}
+        # context["cam_frames"] = service.gen_frames(cam_id)
+
+        return render(request, self.template_name, context=context)
+
+def stream(request, id):
+    return StreamingHttpResponse(service.gen_frames(id), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+
+        
+
